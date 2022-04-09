dce68dfe7bed539be66e72dcd74f674fb928fe29
Nanxi Chen <1367248805@qq.com>
2022-04-10 01:44:12 +0800

    add 0003

---
+++ 0003.Longest Substring Without Repeating Characters/Program.cs
@@ -0,0 +1,52 @@
+﻿
+
+var s = "abba";
+var solution = new Solution();
+var r = solution.LengthOfLongestSubstring(s);
+System.Console.WriteLine(r);
+
+public class Solution
+{
+    public int LengthOfLongestSubstring_origin(string s)
+    {
+        var dict = new Dictionary<char, int>();
+        var longest = 0;
+        var count = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (dict.ContainsKey(s[i]))
+            {
+                if (longest < count) longest = count;
+                count = 0;
+                i = dict[s[i]];
+                dict.Clear();
+            }
+            else
+            {
+                dict[s[i]] = i;
+                count++;
+            }
+        }
+        return longest < count ? count : longest;
+    }
+
+    public int LengthOfLongestSubstring(string s)
+    {
+        var dict = new Dictionary<char, int>();
+        int longest = 0, start = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (dict.ContainsKey(s[i]))
+            {
+                if (longest < i - start) longest = i - start;
+                if (dict[s[i]] + 1 > start) start = dict[s[i]] + 1;
+                dict[s[i]] = i;
+            }
+            else
+            {
+                dict[s[i]] = i;
+            }
+        }
+        return longest < s.Length - start ? s.Length - start : longest;
+    }
+}

a25c75acec76e86dbd2a0980dc5246afa9d149a9
Nanxi Chen <1367248805@qq.com>
2022-04-10 00:25:51 +0800

    add 0001 & 0002

---
+++ 0001.Two Sum/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var dict = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var has = dict.ContainsKey(nums[i]);    // If current number satisfy the difference somewhere?
+            if (has) { 
+                return new[] { i, dict[nums[i]] };  
+            }
+            dict[target - nums[i]] = i;     // Store the difference and index
+        }
+        return new[] { 0, 0 };
+    }
+}
---
+++ 0002.Add Two Numbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+
+
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+
+
+public class Solution
+{
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        ListNode ans = new ListNode();
+        var pointer = ans;
+        int carry = 0;
+        while (true)
+        {
+            pointer.val = ((l1 is null ? 0 : l1.val) + (l2 is null ? 0 : l2.val) + carry);
+            carry = pointer.val < 10 ? 0 : 1;
+            pointer.val %= 10;
+            l1 = l1?.next;
+            l2 = l2?.next;
+            if(l1 is null && l2 is null) break;
+            pointer.next = new ListNode();
+            pointer = pointer.next;
+        }
+        if(carry==1)    pointer.next=new ListNode(carry );
+        return ans;
+    }
+}

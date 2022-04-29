f9ef0011c98b72671d39d9547d532889a72f12b4
Nanxi Chen <1367248805@qq.com>
2022-04-30 05:16:57 +0800

    add 0215 & extract cs file to root folder

---
+++ 0215.Kth Largest Element in an Array.cs
@@ -0,0 +1,21 @@
+﻿
+
+var nums = new int[]{3,2,1,5,6,4};
+var k = 2;
+System.Console.WriteLine(new Solution().FindKthLargest(nums,k));
+
+public class Solution
+{
+    public int FindKthLargest(int[] nums, int k)
+    {
+        PriorityQueue<int, int> pq = new();
+        foreach (var n in nums)
+        {
+            pq.Enqueue(n,n);
+            if(pq.Count>k){
+                pq.Dequeue();
+            }
+        }
+        return pq.Dequeue();
+    }
+}

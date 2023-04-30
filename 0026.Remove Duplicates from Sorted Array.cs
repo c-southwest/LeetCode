5c1dfc8f2cf8a31717dd9ce15c813b1422bf2bdc
Nanxi Chen <1367248805@qq.com>
2023-04-30 16:24:54 +0800

    new solution

---
+++ 0026.Remove Duplicates from Sorted Array.cs
@@ -0,0 +1,34 @@
+﻿int[] arr = new int[] { 1, 2, 2 };
+
+int r = new Solution().RemoveDuplicates(arr);
+Console.WriteLine(r);
+Console.WriteLine();
+foreach (int i in arr)
+{
+    Console.WriteLine(i);
+}
+
+public class Solution
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        if (nums.Length <= 1)
+        {
+            return nums.Length;
+        }
+        int fast = 1, slow = 0;
+        while (fast < nums.Length)
+        {
+
+            if (nums[fast] == nums[slow])
+            {
+                fast++;
+            }
+            else
+            {
+                nums[++slow] = nums[fast++];
+            }
+        }
+        return slow+1;
+    }
+}

3f5649de26a28573a0f325c001642fff89a83d67
Nanxi Chen <41471673+c-southwest@users.noreply.github.com>
2024-02-14 11:31:26 +0100

    Create 0146.LRU Cache.java

---
+++ 0146.LRU Cache.java
@@ -0,0 +1,75 @@
+class LRUCache {
+
+    class DLinkedNode{
+        int key;
+        int value;
+        DLinkedNode prev;
+        DLinkedNode next;
+        public DLinkedNode(){}
+        public DLinkedNode(int _key, int _value){
+            key = _key;
+            value = _value;
+        }
+    }
+    
+    Map<Integer, DLinkedNode> cache  = new HashMap<Integer, DLinkedNode>();
+    int capacity=0;
+    DLinkedNode head, tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        head = new DLinkedNode();
+        tail = new DLinkedNode();
+        head.next = tail;
+        tail.prev = head;
+    }
+    
+    public int get(int key) {
+        DLinkedNode node = cache.get(key);
+        if (node == null) {
+            return -1;
+        }
+        moveToHead(node);
+        return node.value;
+    }
+    
+    public void put(int key, int value) {
+        DLinkedNode node = cache.get(key);
+        if(node == null){
+            DLinkedNode newNode = new DLinkedNode(key, value);
+            cache.put(key, newNode);
+            addToHead(newNode);
+            if(cache.size()>capacity){
+                DLinkedNode tail = removeTail();
+                cache.remove(tail.key);
+            }
+        }
+        else{
+            node.value = value;
+            moveToHead(node);
+        }
+    }
+
+    private void addToHead(DLinkedNode node) {
+        node.prev = head;
+        node.next = head.next;
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    private void removeNode(DLinkedNode node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void moveToHead(DLinkedNode node) {
+        removeNode(node);
+        addToHead(node);
+    }
+
+    private DLinkedNode removeTail() {
+        DLinkedNode res = tail.prev;
+        removeNode(res);
+        return res;
+    }
+}
